4e490c9a700ea1c2c7c4d74591005ea3871d9b70
MikyGus <mikygus@gmail.com>
2022-10-02 10:41:23 +0200

    feature: Modify creation of database
    Modify the sql-file that creates the initial database
    
    Better naming of the table with users

--- Docker_MySql/Init/01.sql
+++ Docker_MySql/Init/01.sql
@@ -1,12 +1,13 @@
-CREATE TABLE users (
+CREATE TABLE auth_users (
     id VARCHAR(255),
     username VARCHAR(255),
     normalizedusername VARCHAR(255)
 );
 
-INSERT INTO users(id, username, normalizedusername) VALUES
+/*Seeding the database*/
+INSERT INTO auth_users(id, username, normalizedusername) VALUES
 ('647b80f0-8f62-4700-a047-7c9f708a6931','Miky','miky'),
-('1abff9ba-b1b3-4477-8303-0408c0fb8301','Bob','bob'),
+('1abff9ba-b1b3-4477-8303-0408c0fb8301','Bill','bill'),
 ('2aee661c-bb27-4c0b-829d-7e1084e075ae','Charlie','charlie'),
 ('247b1e56-4f34-48b9-89b5-20d2608ddf90','Sera','sera');
 
